036f226c00db17d47ae2fd28b23845b215877dfd
babblebey <babblebey@gmail.com>
2025-10-25 01:48:49 +0100

    Refactor pull request handling to use GitHub API for file retrieval

--- genaisrc/prr.genai.mjs
+++ genaisrc/prr.genai.mjs
@@ -7,18 +7,33 @@ script({
     },
 })
 const { dbg, vars } = env
-const base = vars.base || (await git.defaultBranch())
-const changes = await git.diff({
-    base,
-    llmify: true,
-})
-if (!changes) cancel("No changes found in the pull request")
-dbg(`changes: %s`, changes)
-const gitDiff = def("GIT_DIFF", changes, {
-    language: "diff",
-    maxTokens: 14000,
-    detectPromptInjection: "available",
-})
+// const base = vars.base || (await git.defaultBranch())
+// const changes = await git.diff({
+//     base,
+//     llmify: true,
+// })
+// if (!changes) cancel("No changes found in the pull request")
+// dbg(`changes: %s`, changes)
+
+const pull_request = await github.getPullRequest();
+// await github.createReaction("pull_request", pull_request?.number || env.vars.pull_request_number, "eyes");
+const { client } = await github.api();
+  
+const { data: files } = await client.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files', {
+  owner: 'babblebey',
+  repo: 'test',
+  pull_number: pull_request?.number,
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+});
+
+const gitDiff = def("GIT_DIFF", files[0].patch, {
+  language: "diff",
+  maxTokens: 14000,
+  detectPromptInjection: "available",
+});
+
 $`Report errors in ${gitDiff} using the annotation format.
 
 - Use best practices of the programming language of each file.
